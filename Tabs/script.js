c47002eb9869e5d4ba9d2f58e13ee6ca57c41b46
qoyum-eng <qoyumayinla49@gmail.com>
2026-09-19 23:42:23 +0100

    Tabs

---
+++ Tabs/script.js
@@ -0,0 +1,8 @@
+function openTab(tabName) {
+    let tabs = document.querySelectorAll(".tab-content");
+   
+    tabs.forEach(funtion(tab) {
+        tabs.style.display = "none";
+    });
+    document.getElementById(tabName).style.display = "block";
+}
